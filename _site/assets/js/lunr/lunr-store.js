49c0cfa37bf08ce227fd1c1ea2582356b15f4641
QUIQQY <evan.donald@proton.me>
2025-11-09 01:35:44 +0000

    byhuvyu

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "After Hours - The Weeknd",
-        "excerpt":"  ","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/jekyll/update/2025/11/09/afterhourstheweeknd.html",
-        "teaser": null
-      }]
